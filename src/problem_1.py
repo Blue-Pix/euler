7ee702d537ee1c673a79b084a8f7bfffc4852ea2
sakuraya <sakuraya@blue-pixel.info>
2018-02-04 21:24:52 +0900

    add japanese translation to Problem 1.

--- src/problem_1.py
+++ src/problem_1.py
@@ -5,6 +5,8 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5,
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
+
+3または5で割り切れる1000未満の自然数の和を求める
 '''
 
 import numpy as np
