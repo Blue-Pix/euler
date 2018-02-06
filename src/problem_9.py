81c5c63a345f4a95beca069b98017b871e87a0bb
sakuraya <sakuraya@blue-pixel.info>
2018-02-06 14:56:48 +0900

    Problem 9 refactored.

--- src/problem_9.py
+++ src/problem_9.py
@@ -15,27 +15,26 @@ a + b + c = 1000 になるようなピタゴラス数の組み合わせを求め
 import numpy as np
 
 
+def is_square_number(num):
+    return True if np.sqrt(num) % 1 == 0 else False
+
+
 if __name__ == '__main__':
-    # square
     src = np.array([i ** 2 for i in range(1, 1000)])
-    found = False
-    for i in range(0, len(src)):
-        for j in range(i, len(src)):
-            a = src[i]
-            b = src[j]
-            c = np.sqrt(a + b)
-            # a**2 + b**2 = c**2 ?
-            if c % 1 != 0:
+    answer = None
+    length = len(src)
+    for i in range(0, length):
+        for j in range(i, length):
+            a, b = src[i], src[j]
+            if is_square_number(a + b) is False:
                 continue
+            c = np.sqrt(a + b)
             a = np.sqrt(a)
             b = np.sqrt(b)
-            # a + b + c = 1000 ?
-            if a + b + c != 1000:
-                continue
-
-            print('Pythagorean triplet is: %d**2 + %d**2 = %d' % (a, b, c))
-            print('%d * %d * %d = %d' % (a, b, c, a * b * c))
-            found = True
-
-        if found is True:
+            if (a + b + c == 1000) & (a < b < c):
+                print('Pythagorean triplet is: %d**2 + %d**2 = %d**2' % (a, b, c))
+                answer = a * b * c
+                break
+        if answer is not None:
             break
+    print('product abc is %d' % answer)
