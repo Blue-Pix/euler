11273139b9b3d7844337b441847e779e61f727ba
sakuraya <sakuraya@blue-pixel.info>
2018-02-09 10:19:37 +0900

    Problem 11 refactored.

--- src/10-19/problem_11.py
+++ src/10-19/problem_11.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
   DIMENSION = 4
   greatest_product = 0
 
-  src = src.lstrip().rstrip().replace('\n', '').split(' ')
+  src = src.replace('\n', '').split(' ')
   src = filter(lambda i: i != '', src)
   src = np.array(src).reshape(ROW, COLUMN)
   src = np.append(src, np.zeros((DIMENSION, COLUMN)), axis=0)
@@ -71,9 +71,9 @@ if __name__ == '__main__':
   for i in range(0, ROW - DIMENSION + 1):
     for j in range(0, COLUMN - DIMENSION + 1):
       dest = src[i:i + DIMENSION, j:j + DIMENSION]
-      h_product = reduce(lambda x, y: int(x) * int(y), dest[0])
-      v_product = reduce(lambda x, y: int(x) * int(y), dest.T[0])
-      d_product = reduce(lambda x, y: int(x) * int(y), np.diag(dest))
+      h_product  = reduce(lambda x, y: int(x) * int(y), dest[0])
+      v_product  = reduce(lambda x, y: int(x) * int(y), dest.T[0])
+      d_product  = reduce(lambda x, y: int(x) * int(y), np.diag(dest))
       rd_product = reduce(lambda x, y: int(x) * int(y), np.diag(dest[::, ::-1]))
 
       greatest_product = max([h_product, v_product, d_product, rd_product, greatest_product])
