0bc2b2f8d90ff87506f9aca295568d8241fbd7d5
sakuraya <sakuraya@blue-pixel.info>
2018-02-11 05:30:03 +0900

    Problem 16 solved.

---
+++ src/10-19/problem_16.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+''' Problem 16
+2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2**1000?
+
+2の1,000乗の各桁の総和を求める
+'''
+
+
+import time
+
+
+if __name__ == '__main__':
+	src = 2 ** 1000
+	_sum = reduce(lambda x, y: int(x) + int(y), str(src))
+	print('answer is %d' % _sum)
